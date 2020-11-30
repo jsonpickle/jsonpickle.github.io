20c77d0722a2ca2cbbb186ceb8df38b4fa4fa6d5
David Aguilar <davvid@gmail.com>
2020-11-29 19:19:13 -0800

    jsonpickle v1.4.2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0',
+    VERSION: '1.4.3.dev0+g5b8d3ea.d20201130',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
 };
